c75432af986a2a57c0e4a9b9f56ab7d9d5a040e0
Mengsen-W <mengsen_wang@163.com>
2020-02-10 13:21:45 +0800

    🔗

---
+++ ElasticColumn/elasticColumn.tcl
@@ -0,0 +1,8 @@
+# * @Author: Mengsen.Wang
+# * @Date: 2020-02-10 13:18:46
+# * @Last Modified by:   Mengsen.Wang
+# * @Last Modified time: 2020-02-10 13:18:46
+
+# 该算例模型为弹性混凝土柱
+# 三节点两单元 一端固定
+# 截面积大小 为 0.5m * 0.5m E = 3e8
