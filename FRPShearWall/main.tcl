e7bc9373333f32e10bebae20659f3f02ad947151
Mengsen-W <mengsen_wang@163.com>
2020-02-20 16:35:51 +0800

    Fixbug

--- FRPShearWall/main.tcl
+++ FRPShearWall/main.tcl
@@ -42,7 +42,7 @@ puts "End of Pushover"
 
 # 若 Dnum 设为1，则 Ddelta 为每一圈的最大位移
 # 若 Dnum 不唯一，则 Ddelta 为每圈的增量
-Cyclic_Function 2 35 1 2607 1 1E-2 1000
+Cyclic_Function 2 30 1 2607 1 1E-2 1000
 ModelInfo_Proc model.txt
 puts "\nAll of End\n"
 
