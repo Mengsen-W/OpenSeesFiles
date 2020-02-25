1221ce0b4a1c20f4cacc867f6c83a7f07404c7d4
Mengsen-W <mengsen_wang@163.com>
2020-02-25 12:01:35 +0800

    Trying New Test

---
+++ SFCB-2/Gravity.tcl
@@ -0,0 +1,9 @@
+# * @coding = utf-8
+# * @Decription: Gravity
+# * @Author: Mengsen.Wang
+# * @Date: 2020-02-14 19:30:48
+# * @Last Modified time: 2020-02-14 19:30:48
+
+pattern Plain 1 Linear {
+    load 2607 0 0 -756380 0 0 0
+}
