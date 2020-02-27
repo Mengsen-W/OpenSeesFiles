e9057c0d0b0239aec20dad05419c2dee10295b10
Mengsen-W <mengsen_wang@163.com>
2020-02-27 22:53:13 +0800

    Fix

--- SFCB-2/main-1.tcl
+++ SFCB-2/main-1.tcl
@@ -43,7 +43,7 @@ puts "End of Pushover"
 
 # 若 Dnum 设为1，则 Ddelta 为每一圈的最大位移
 # 若 Dnum 不唯一，则 Ddelta 为每圈的增量
-Cyclic_Function 2 35 0.2 2607 1 1E-2 1000
+Cyclic_Function 2 35 0.5 2607 1 1E-2 1000
 ModelInfo_Proc model-Steel-1.txt
 puts "\nAll of End\n"
 wipe
