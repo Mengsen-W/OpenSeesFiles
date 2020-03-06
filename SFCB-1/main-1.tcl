87958f35896f2b54346b83a631a523320fa2e2e8
Mengsen-W <mengsen_wang@163.com>
2020-03-06 18:14:02 +0800

    Fixbug

--- SFCB-1/main-1.tcl
+++ SFCB-1/main-1.tcl
@@ -17,7 +17,7 @@ source Nodes.tcl
 puts "End of Nodes"
 
 puts "\nMaterial"
-source Material-4-3.tcl
+source Material-4-4.tcl
 puts "End of Material"
 
 puts "\nElement"
@@ -32,7 +32,7 @@ Gravity_Proc 10
 puts "End of Gravity"
 
 puts "\nOutput"
-recorder Node -file F-Disp-4-3.txt -time -node 2607 -dof 1 disp
+recorder Node -file F-Disp-4-4.txt -time -node 2607 -dof 1 disp
 puts "End of Output"
 
 puts "\nPushover"
@@ -43,8 +43,8 @@ puts "End of Pushover"
 
 # 若 Dnum 设为1，则 Ddelta 为每一圈的最大位移
 # 若 Dnum 不唯一，则 Ddelta 为每圈的增量
-Cyclic_Function 2 35 1 2607 1 1E-2 1000
-ModelInfo_Proc model-4-3.txt
+Cyclic_Function 2 35 0.8 2607 1 1E-2 1000
+ModelInfo_Proc model-4-4.txt
 puts "\nAll of End\n"
 wipe
 reset
