0b67be4c17bd54784f7860f909113e489b4b2b28
Mengsen.Wang <mengsen_wang@163.com>
2020-10-01 22:47:41 +0800

    我也不知道哪块错了

--- paper_reproduce/MPASWSRMUO/C30/main.py
+++ paper_reproduce/MPASWSRMUO/C30/main.py
@@ -29,35 +29,35 @@ perprocess.set_uniaxial_metrial()
 perprocess.set_section()
 perprocess.set_element()
 
-ops.recorder('Node', '-file', 'output\\disp.out', '-node', 1008, 2008, 3008, 4008, 5008, 6008, 7008, 8008, '-time',
-             '-dof',  3, 'disp')
-# set timeSeries
-ops.timeSeries('Linear', 1)
-logger.info("[Linear] timeSeries tag 1 created")
+# ops.recorder('Node', '-file', 'output\\disp.out', '-node', 1008, 2008, 3008, 4008, 5008, 6008, 7008, 8008, '-time',
+#              '-dof',  3, 'disp')
+# # set timeSeries
+# ops.timeSeries('Linear', 1)
+# logger.info("[Linear] timeSeries tag 1 created")
 
-# set load pattern
-ops.pattern('Plain', 1, 1)
-load: float = [0, 0, -225000, 0, 0, 0]
-for i in range(1, 8):
-    ops.load(i * 1000 + 8, *load)
-    logger.info(
-        "load node = %d and load = [%f, %f, %f, %f, %f, %f]", i * 1000 + 8, *load)
+# # set load pattern
+# ops.pattern('Plain', 1, 1)
+# load: float = [0, 0, -225000, 0, 0, 0]
+# for i in range(1, 8):
+#     ops.load(i * 1000 + 8, *load)
+#     logger.info(
+#         "load node = %d and load = [%f, %f, %f, %f, %f, %f]", i * 1000 + 8, *load)
 
-# opsplt.createODB("ShearWall", 'LoadCaseName')
-# load analyze
-ops.constraints('Plain')
-ops.numberer('Plain')
-ops.system('BandGen')
-ops.test('NormDispIncr', 1.0e-6, 200)
-ops.algorithm('Newton')
-ops.integrator('LoadControl', 0.01)
-ops.analysis('Static')
-ok = ops.analyze(100)
-logger.info("gravity analyze result is %s", ok == 0)
+# # opsplt.createODB("ShearWall", 'LoadCaseName')
+# # load analyze
+# ops.constraints('Plain')
+# ops.numberer('Plain')
+# ops.system('BandGen')
+# ops.test('NormDispIncr', 1.0e-6, 200)
+# ops.algorithm('Newton')
+# ops.integrator('LoadControl', 0.01)
+# ops.analysis('Static')
+# ok = ops.analyze(100)
+# logger.info("gravity analyze result is %s", ok == 0)
 
-ops.remove('recorders')
-ops.wipeAnalysis()
-ops.loadConst('-time', 0.0)
+# ops.remove('recorders')
+# ops.wipeAnalysis()
+# ops.loadConst('-time', 0.0)
 
 # set hysteresis
 # ops.recorder('Node', '-file', 'output\\node1008.out', '-time', '-node', 1008,'-dof', 1, 'disp')
@@ -82,8 +82,35 @@ ops.recorder('Node', '-file', 'output\\rea_cy.out', '-node', 1008, '-time', '-pr
              '-dof',  1, 'reaction')
 ops.pattern('Plain', 200, 2)
 ops.load(1008, 1, 0, 0, 0, 0, 0)
-CyclicDisplace(1e-3, 10, 1e-3, 1008, 1, 1E-2, 1000)
-
+# CyclicDisplace(1e-3, 10, 1e-3, 1008, 1, 1E-2, 1000)
+ops.constraints('Penalty', 1e20, 1e20)
+ops.numberer('RCM')
+ops.system('BandGeneral')
+ops.test('NormDispIncr', 1.0e-2, 1000)
+ops.integrator('DisplacementControl', 1008, 1, 1)
+ops.algorithm('KrylovNewton')
+ops.analysis('Static')
+ops.analyze(5)
+ops.integrator('DisplacementControl', 1008, 1, -1)
+ops.algorithm('KrylovNewton')
+ops.analysis('Static')
+ops.analyze(10)
+ops.integrator('DisplacementControl', 1008, 1, 1)
+ops.algorithm('KrylovNewton')
+ops.analysis('Static')
+ops.analyze(5)
+ops.integrator('DisplacementControl', 1008, 1, 1)
+ops.algorithm('KrylovNewton')
+ops.analysis('Static')
+ops.analyze(10)
+ops.integrator('DisplacementControl', 1008, 1, -1)
+ops.algorithm('KrylovNewton')
+ops.analysis('Static')
+ops.analyze(20)
+ops.integrator('DisplacementControl', 1008, 1, 1)
+ops.algorithm('KrylovNewton')
+ops.analysis('Static')
+ops.analyze(10)
 
 if __name__ == "__main__":
     # for debug
